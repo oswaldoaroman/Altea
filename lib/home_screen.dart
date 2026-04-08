6532144af7287cdf834184a1c09c003379797965
Mr-ALG0 <cristobalalg0.tlc@gmail.com>
2026-04-07 23:14:16 -0600

    Implement HomeScreen with sections for informes and voice

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,3 +1,24 @@
+import 'package:flutter/material.dart';
 import 'widgets/informes_section.dart';
 import 'widgets/voice_section.dart';
 import 'widgets/medico_section.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFE1F0F7),
+      appBar: AppBar(
+        title: Text("Informes y Servicios"),
+        backgroundColor: Color(0xFF004A99),
+      ),
+      body: ListView(
+        children: [
+          InformesSection(),
+          VoiceSection(),
+          MedicoSection(),
+        ],
+      ),
+    );
+  }
+}
